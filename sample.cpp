8d4add02ca4c0cba0cd59f5cd6fff67e59c33974
Konvt <iftiy@foxmail.com>
2024-01-01 17:57:55 +0800

    fix bugs

--- sample.cpp
+++ sample.cpp
@@ -1,10 +1,12 @@
 #include <vector>
 #include "pgbar/range.hpp"
+double origin_arr[100000000] {0.0};
 
 int main()
 {
     std::size_t TOTAL = 100000000; // The maximum task load without seriously slowing down the program.
 
+    std::cout << "Testing...\n";
     pgbar::pgbar bar; bar.set_task(TOTAL).set_left_bracket(" ").set_right_bracket("").set_todo_char("\033[31m━\033[0m").set_done_char("\033[32m━\033[0m");
     bar.set_style(pgbar::style_opts::entire).set_step(2); // Set the step.
 
@@ -31,6 +33,11 @@ int main()
     std::cout << "Pointer arrays is okay: ";
     for (auto iter : pgbar::range(pointer_arr+TOTAL-1, pointer_arr-1, bar))
         continue; // Also can pass a pointer array as the range
+    delete[] pointer_arr; pointer_arr = nullptr;
 
-    delete[] pointer_arr; return 0;
+    bar.reset().set_style(pgbar::style_opts::entire);
+    for (auto ele : pgbar::range(origin_arr, bar))
+        continue;
+    
+    return 0;
 }
