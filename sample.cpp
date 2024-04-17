b1fc9a77dc60623b49472dfb7cc248f9c0becdb9
Konvt <120050092+Konvt@users.noreply.github.com>
2024-04-17 15:25:50 +0800

    1. Output stream objects and renderer types are now considered part of the type parameters of the pgbar object. 2. Added an optional single-threaded renderer. 3. Introduced some type predicates such as 'is_pgbar', 'is_renderer', etc. 4. Increased type constraint requirements for the range function. 5. Adjusted code structure. 6. Added color highlighting to error messages for static assertions. 7. TODO: Currently, there are rendering issues with the pgbar's visual effects, to be fixed.

--- sample.cpp
+++ sample.cpp
@@ -1,40 +1,41 @@
 #include <vector>
 #include "include/pgbar/range.hpp"
-constexpr std::size_t TOTAL = 100000000;
-double origin_arr[500] {0.0};
+constexpr std::size_t TOTAL = 10000;
+double origin_arr[500] { 0.0 };
 
 int main()
 {
   std::cout << "Testing...\n";
-  pgbar::pgbar<> bar; bar.set_task(TOTAL).set_left_bracket(" ").set_right_bracket("").set_todo_char("\033[31m━\033[0m").set_done_char("\033[32m━\033[0m");
-  bar.set_style(pgbar::style::entire).set_step(2); // Set the step.
+  pgbar::pgbar<std::ostream, pgbar::singlethread> bar { TOTAL, std::cerr };
+  bar.set_left_bracket( " " ).set_right_bracket( "" );//.set_todo_char( "\033[31m━\033[0m" ).set_done_char( "\033[32m━\033[0m" );
+  bar.set_style( pgbar::style::entire ).set_step( 2 ); // Set the step.
   for ( std::size_t i = 0; i < (TOTAL / 2); ++i ) {
     bar.update(); // Normal update
     // Do anything you want here...
   }
-
+  /*
   bar.reset().set_style( pgbar::style::percentage | pgbar::style::task_counter | pgbar::style::countdown );
   std::cout << "Task progress: "; // `range` needs a progress bar to show the progress situation
-  for (auto ele : pgbar::range(TOTAL / 2, bar))
+  for ( auto ele : pgbar::range( TOTAL / 2, bar ) )
     continue; // You can specify a range using `range`
   // The iterator will automatically count the number of tasks
 
   std::vector<double> arr {};
-  for (std::size_t i = 0; i < 5000; ++i)
-    arr.push_back(i);
-  bar.reset().set_style(pgbar::style::entire & ~pgbar::style::bar);
-  for (auto ele : pgbar::range(arr, bar))
+  for ( std::size_t i = 0; i < 5000; ++i )
+    arr.push_back( i );
+  bar.reset().set_style( pgbar::style::entire & ~pgbar::style::bar );
+  for ( auto ele : pgbar::range( arr, bar ) )
     continue; // Using a container with elements as the range
-  arr.resize(0);
+  arr.resize( 0 );
 
-  float *pointer_arr = new float[TOTAL] {0.0};
-  bar.reset().set_style(pgbar::style::task_counter);
+  float* pointer_arr = new float[TOTAL] {0.0};
+  bar.reset().set_style( pgbar::style::task_counter );
   std::cout << "Pointer arrays is okay: ";
-  for (auto ele : pgbar::range(pointer_arr + TOTAL - 1, pointer_arr - 1, bar))
+  for ( auto ele : pgbar::range( pointer_arr + TOTAL - 1, pointer_arr - 1, bar ) )
     continue; // Also can pass a pointer array as the range
   delete[] pointer_arr; pointer_arr = nullptr;
 
-  bar.reset().set_style(pgbar::style::entire);
-  for (auto ele : pgbar::range(origin_arr, bar))
-    continue;
+  bar.reset().set_style( pgbar::style::entire );
+  for ( auto ele : pgbar::range( origin_arr, bar ) )
+    continue;*/
 }
