9e3d9ef7aaa80dded627573c7b6e49ed755b0b49
Konvt <120050092+Konvt@users.noreply.github.com>
2025-02-17 01:40:37 +0800

    fixed typo

--- demo/blockprogressbar.cpp
+++ demo/blockprogressbar.cpp
@@ -3,7 +3,7 @@
 int main()
 {
   pgbar::BlockProgressBar<> bpbar;
-  bpbar.config().style( pgbar::config::SpinBar::Entire ).tasks( 2147483647 );
+  bpbar.config().style( pgbar::config::BlckBar::Entire ).tasks( 2147483647 );
   for ( size_t i = 0; i < 2147483647; ++i )
     bpbar.tick();
 }
--- demo/scannerbar.cpp
+++ demo/scannerbar.cpp
@@ -3,7 +3,7 @@
 int main()
 {
   pgbar::ScannerBar<> scnbar;
-  scnbar.config().style( pgbar::config::SpinBar::Entire ).tasks( 2147483647 );
+  scnbar.config().style( pgbar::config::ScanBar::Entire ).tasks( 2147483647 );
   for ( size_t i = 0; i < 2147483647; ++i )
     scnbar.tick();
 }
