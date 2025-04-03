4d3e14c270dff1cdeb20d89fff6961faeb093a35
Konvt <120050092+Konvt@users.noreply.github.com>
2025-04-03 21:47:01 +0800

    1. Rewrote the content of QuickStart; 2. Added a new demonstration GIF and sample program multibar.cpp; 3. Fixed several issues.

--- demo/blockprogressbar.cpp
+++ demo/blockprogressbar.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+  #if _WIN32
+    system( "chcp 65001" );
+  #endif
+  
   pgbar::BlockProgressBar<> bpbar;
   bpbar.config().style( pgbar::config::BlckBar::Entire ).tasks( 2147483647 );
   for ( size_t i = 0; i < 2147483647; ++i )
---
+++ demo/multibar.cpp
@@ -0,0 +1,37 @@
+#include "pgbar/pgbar.hpp"
+#include <chrono>
+#include <random>
+#include <thread>
+using namespace std;
+
+int main()
+{
+#if _WIN32
+  system( "chcp 65001" );
+#endif
+
+  auto mbar = pgbar::make_multi<3, pgbar::BlockProgressBar<>>();
+
+  vector<thread> pool;
+  pool.emplace_back( thread( [&mbar]() {
+    mbar.iterate<0>( 500, []( int ) {
+      auto rd = mt19937( random_device {}() );
+      this_thread::sleep_for( chrono::milliseconds( uniform_int_distribution<>( 10, 70 )( rd ) ) );
+    } );
+  } ) );
+  pool.emplace_back( thread( [&mbar]() {
+    mbar.config<1>().tasks( 300 );
+    auto rd = mt19937( random_device {}() );
+    for ( int i = 0; i < 300; ++i ) {
+      mbar.tick<1>();
+      this_thread::sleep_for( chrono::milliseconds( uniform_int_distribution<>( 20, 120 )( rd ) ) );
+    }
+  } ) );
+  pool.emplace_back( thread( [&mbar]() { mbar.iterate<2>( 2147483647, []( int ) {} ); } ) );
+
+  for ( auto& td : pool ) {
+    if ( td.joinable() )
+      td.join();
+  }
+  mbar.wait();
+}
