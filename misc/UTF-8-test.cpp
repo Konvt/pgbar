b0b125cecbad08f8ed3922e9db5f488dca10d3de
Konvt <120050092+Konvt@users.noreply.github.com>
2025-08-04 01:44:48 +0800

    restored the deleted macros to simplify the program logic

--- misc/UTF-8-test.cpp
+++ misc/UTF-8-test.cpp
@@ -1,4 +1,3 @@
-#include <cstdint>
 #include <iostream>
 #include <stdexcept>
 #include <string>
---
+++ misc/main.cpp
@@ -0,0 +1,21 @@
+// This file is only used to trigger CMake to generate compile_commands.json.
+#include "pgbar/pgbar.hpp"
+#include <random>
+#include <thread>
+using namespace std;
+
+int main()
+{
+#if _WIN32
+  system( "chcp 65001" );
+#endif
+
+  pgbar::iterate<pgbar::config::Block>(
+    10000,
+    []( int ) {
+      mt19937 rd { random_device {}() };
+      this_thread::sleep_for( chrono::microseconds( uniform_int_distribution<int>( 700, 1100 )( rd ) ) );
+    },
+    pgbar::option::Lead( { " ", "▁", "▂", "▃", "▃", "▄", "▅", "▆", "▇", "█" } ),
+    pgbar::option::InfoColor( "#FFD200" ) );
+}
