ec71b146fd7f07d3cdc977fae61c0747e61d3a04
Konvt <120050092+Konvt@users.noreply.github.com>
2025-09-03 16:35:58 +0800

    now the types in pgbar::option are allowed the default construction to represent passing an empty parameter

--- demo/multibar.cpp
+++ demo/multibar.cpp
@@ -9,24 +9,51 @@ int main()
 #if _WIN32
   system( "chcp 65001" );
 #endif
-  auto mbar = pgbar::make_multi<pgbar::BlockBar<>, 3>();
+
+  auto mbar = pgbar::make_multi(
+    pgbar::config::Flow( pgbar::option::Style( pgbar::config::Flow::Entire ),
+                         pgbar::option::Filler( "━" ),
+                         pgbar::option::FillerColor( pgbar::color::Red ),
+                         pgbar::option::Lead( "━━" ),
+                         pgbar::option::LeadColor( pgbar::color::White ),
+                         pgbar::option::InfoColor( "#F5B0B6" ),
+                         pgbar::option::Starting(),
+                         pgbar::option::Ending() ),
+    pgbar::config::Block( pgbar::option::Lead( { " ", "▁", "▂", "▃", "▄", "▅", "▆", "▇" } ),
+                          pgbar::option::InfoColor( "#F7A699" ) ),
+    pgbar::config::Block( pgbar::option::Filler( "⠿" ),
+                          pgbar::option::Lead( { " ", "⠄", "⠆", "⠇", "⠧", "⠷" } ),
+                          pgbar::option::InfoColor( "#7DD4DF" ) ),
+    pgbar::config::Block( pgbar::option::Lead( { " ", "▖", "▞", "▛" } ),
+                          pgbar::option::InfoColor( "#8AB7EB" ) ) );
+  mbar.config<0>().tasks( ( std::tuple_size<decltype( mbar )>::value - 1 ) * 2 );
 
   vector<thread> pool;
-  pool.emplace_back( [&mbar]() {
-    mbar.iterate<1>( 500, []( int ) {
-      auto rd = mt19937( random_device {}() );
-      this_thread::sleep_for( chrono::milliseconds( uniform_int_distribution<>( 10, 70 )( rd ) ) );
-    } );
-  } );
-  pool.emplace_back( [&mbar]() {
-    mbar.config<2>().tasks( 300 );
-    auto rd = mt19937( random_device {}() );
-    for ( int i = 0; i < 300; ++i ) {
-      mbar.tick<2>();
-      this_thread::sleep_for( chrono::milliseconds( uniform_int_distribution<>( 20, 120 )( rd ) ) );
+  pool.emplace_back( [&]() {
+    mt19937 rd { random_device {}() };
+    mbar.config<1>().tasks( 30000 );
+    mbar.tick<0>();
+    for ( size_t i = 0; i < 30000; ++i ) {
+      mbar.tick<1>();
+      this_thread::sleep_for( chrono::microseconds( uniform_int_distribution<>( 800, 1300 )( rd ) ) );
     }
+    mbar.tick<0>();
   } );
-  pool.emplace_back( [&mbar]() { mbar.iterate<0>( 2147483647, []( int ) {} ); } );
+  pool.emplace_back( [&]() {
+    mt19937 rd { random_device {}() };
+    mbar.tick<0>();
+    mbar.iterate<2>( 10000, [&rd]( int ) {
+      this_thread::sleep_for( chrono::milliseconds( uniform_int_distribution<>( 1, 8 )( rd ) ) );
+    } );
+    mbar.tick<0>();
+  } );
+  pool.emplace_back( [&]() {
+    mt19937 rd { random_device {}() };
+    mbar.tick<0>();
+    for ( auto _ : mbar.iterate<3>( 80000 ) )
+      this_thread::sleep_for( chrono::microseconds( uniform_int_distribution<>( 700, 1100 )( rd ) ) );
+    mbar.tick<0>();
+  } );
 
   for ( auto& td : pool ) {
     if ( td.joinable() )
