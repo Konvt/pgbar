a77bfbb6cb8b25675ed10c57d86c052212783612
Konvt <120050092+Konvt@users.noreply.github.com>
2025-12-26 03:38:36 +0800

    modified the synchronization method of the atomic state machine in the renderer

--- include/pgbar/details/concurrent/Util.hpp
+++ include/pgbar/details/concurrent/Util.hpp
@@ -3,14 +3,15 @@
 
 #include "../core/Core.hpp"
 #include "../types/Types.hpp"
-#include <chrono>
+#include <atomic>
 #include <thread>
 
 namespace pgbar {
   namespace _details {
     namespace concurrent {
+      // spin with specified action
       template<typename F, typename Act>
-      PGBAR__FORCEINLINE void spin_while( F&& pred, Act&& action, types::Size threshold )
+      PGBAR__FORCEINLINE void spin_with( F&& pred, Act&& action, types::Size threshold )
         noexcept( noexcept( pred() ) && noexcept( action() ) )
       {
         for ( types::Size cnt = 0; !pred(); ) {
@@ -20,10 +21,10 @@ namespace pgbar {
         }
       }
       template<typename F, typename Act, typename Rep, typename Period>
-      PGBAR__FORCEINLINE bool spin_while_for( F&& pred,
-                                              Act&& action,
-                                              types::Size threshold,
-                                              const std::chrono::duration<Rep, Period>& timeout )
+      PGBAR__FORCEINLINE bool spin_with_for( F&& pred,
+                                             Act&& action,
+                                             types::Size threshold,
+                                             const std::chrono::duration<Rep, Period>& timeout )
         noexcept( noexcept( pred() ) && noexcept( action() ) )
       {
         const auto start = std::chrono::steady_clock::now();
@@ -41,7 +42,7 @@ namespace pgbar {
       template<typename F>
       PGBAR__FORCEINLINE void spin_wait( F&& pred, types::Size threshold ) noexcept( noexcept( pred() ) )
       {
-        spin_while( std::forward<F>( pred ), []() noexcept { std::this_thread::yield(); }, threshold );
+        spin_with( std::forward<F>( pred ), []() noexcept { std::this_thread::yield(); }, threshold );
       }
       template<typename F>
       PGBAR__FORCEINLINE void spin_wait( F&& pred ) noexcept( noexcept( pred() ) )
@@ -55,7 +56,7 @@ namespace pgbar {
                                              const std::chrono::duration<Rep, Period>& timeout )
         noexcept( noexcept( pred() ) )
       {
-        return spin_while_for(
+        return spin_with_for(
           std::forward<F>( pred ),
           []() noexcept { std::this_thread::yield(); },
           threshold,
@@ -66,6 +67,46 @@ namespace pgbar {
         noexcept( noexcept( pred() ) )
       {
         return spin_wait_for( std::forward<F>( pred ), 128, timeout );
+      }
+
+      template<typename T>
+      PGBAR__FORCEINLINE void atomic_commit_all(
+        std::atomic<T>& atom,
+        T alter,
+        std::memory_order order = std::memory_order_release ) noexcept
+      {
+        atom.store( alter, order );
+#ifdef __cpp_lib_atomic_wait
+        atom.notify_all();
+#endif
+      }
+      template<typename T>
+      PGBAR__FORCEINLINE bool atomic_commit_all(
+        std::atomic<T>& atom,
+        T expected,
+        T alter,
+        std::memory_order order = std::memory_order_release ) noexcept
+      {
+        bool cas = atom.compare_exchange_strong( expected, alter, order );
+#ifdef __cpp_lib_atomic_wait
+        if ( cas )
+          atom.notify_all();
+#endif
+        return cas;
+      }
+      template<typename T>
+      PGBAR__FORCEINLINE bool atomic_commit_one(
+        std::atomic<T>& atom,
+        T expected,
+        T alter,
+        std::memory_order order = std::memory_order_release ) noexcept
+      {
+        bool cas = atom.compare_exchange_strong( expected, alter, order );
+#ifdef __cpp_lib_atomic_wait
+        if ( cas )
+          atom.notify_one();
+#endif
+        return cas;
       }
     } // namespace concurrent
   } // namespace _details
--- include/pgbar/details/render/Renderer.hpp
+++ include/pgbar/details/render/Renderer.hpp
@@ -7,8 +7,10 @@
 #include "../utils/ScopeGuard.hpp"
 #include "../wrappers/UniqueFunction.hpp"
 #include <atomic>
-#include <condition_variable>
 #include <thread>
+#ifndef __cpp_lib_atomic_wait
+# include <condition_variable>
+#endif
 
 namespace pgbar {
   namespace _details {
@@ -24,9 +26,11 @@ namespace pgbar {
         wrappers::UniqueFunction<void()> task_ = {};
         std::thread runner_                    = {};
 
+#ifndef __cpp_lib_atomic_wait
         mutable std::condition_variable cond_var_ = {};
-        mutable concurrent::SharedMutex res_mtx_  = {};
-        mutable std::mutex sched_mtx_             = {};
+#endif
+        mutable concurrent::SharedMutex res_mtx_ = {};
+        mutable std::mutex sched_mtx_            = {};
 
         /***********************************************************
           Dead
@@ -56,27 +60,28 @@ namespace pgbar {
           auto guard = utils::make_scope_fail(
             [this]() noexcept { state_.store( State::Dead, std::memory_order_release ); } );
 
-          runner_       = std::thread( [this]() {
+          runner_ = std::thread( [this]() {
             try {
               for ( auto state = state_.load( std::memory_order_acquire ); state != State::Dead;
                     state      = state_.load( std::memory_order_acquire ) ) {
                 switch ( state ) {
-                case State::Asleep: {
-                  auto expected = State::Asleep;
-                  state_.compare_exchange_strong( expected, State::Dormant, std::memory_order_release );
-                }
+                case State::Asleep:
+                  concurrent::atomic_commit_all( state_, State::Asleep, State::Dormant );
                   PGBAR__FALLTHROUGH;
                 case State::Dormant: {
+#ifdef __cpp_lib_atomic_wait
+                  state_.wait( State::Dormant, std::memory_order_acquire );
+#else
                   std::unique_lock<std::mutex> lock { sched_mtx_ };
                   cond_var_.wait( lock, [this]() noexcept {
                     return state_.load( std::memory_order_acquire ) != State::Dormant;
                   } );
+#endif
                 } break;
 
                 case State::Warmup: {
                   task_();
-                  auto expected = State::Warmup;
-                  state_.compare_exchange_strong( expected, State::Loop, std::memory_order_release );
+                  concurrent::atomic_commit_all( state_, State::Warmup, State::Loop );
                 }
                   PGBAR__FALLTHROUGH;
                 case State::Loop: {
@@ -85,14 +90,16 @@ namespace pgbar {
                 } break;
 
                 case State::Primed: {
-                  auto expected = State::Primed;
-                  state_.compare_exchange_strong( expected, State::Pulse, std::memory_order_release );
                   task_();
                   quota_.fetch_sub( 1, std::memory_order_release );
+                  concurrent::atomic_commit_all( state_, State::Primed, State::Pulse );
                 }
                   PGBAR__FALLTHROUGH;
                 case State::Pulse: {
-                  concurrent::spin_while(
+#ifdef __cpp_lib_atomic_wait
+                  quota_.wait( 0, std::memory_order_acquire );
+#else
+                  concurrent::spin_with(
                     [this]() noexcept { return quota_.load( std::memory_order_acquire ) > 0; },
                     [this]() noexcept {
                       std::unique_lock<std::mutex> lock { sched_mtx_ };
@@ -102,6 +109,7 @@ namespace pgbar {
                       } );
                     },
                     1024 );
+#endif
                   do
                     task_();
                   while ( quota_.fetch_sub( 1, std::memory_order_acq_rel ) > 1
@@ -110,30 +118,32 @@ namespace pgbar {
 
                 case State::Shot: {
                   {
-                    std::lock_guard<concurrent::SharedMutex> lock1 { res_mtx_ };
+                    concurrent::SharedLock<concurrent::SharedMutex> lock1 { res_mtx_ };
                     std::lock_guard<std::mutex> lock2 { sched_mtx_ };
                     task_();
                   }
-                  auto expected = State::Shot;
-                  state_.compare_exchange_strong( expected, State::Idle, std::memory_order_release );
+                  concurrent::atomic_commit_all( state_, State::Shot, State::Idle );
                 }
                   PGBAR__FALLTHROUGH;
                 case State::Idle: {
+#ifdef __cpp_lib_atomic_wait
+                  state_.wait( State::Idle, std::memory_order_acquire );
+#else
                   std::unique_lock<std::mutex> lock { sched_mtx_ };
                   cond_var_.wait( lock, [this]() noexcept {
                     return state_.load( std::memory_order_acquire ) != State::Idle;
                   } );
+#endif
                 } break;
 
                 default: return;
                 }
               }
             } catch ( ... ) {
-              if ( !box_.try_store( std::current_exception() ) ) {
-                state_.store( State::Dead, std::memory_order_release );
+              auto dump = box_.try_store( std::current_exception() );
+              concurrent::atomic_commit_all( state_, dump ? State::Dormant : State::Dead );
+              if ( !dump )
                 throw;
-              } else
-                state_.store( State::Dormant, std::memory_order_release );
             }
           } );
           auto expected = State::Dead;
@@ -142,11 +152,7 @@ namespace pgbar {
 
         void shutdown() noexcept
         {
-          state_.store( State::Dead, std::memory_order_release );
-          {
-            std::lock_guard<std::mutex> lock { sched_mtx_ };
-            cond_var_.notify_all();
-          }
+          concurrent::atomic_commit_all( state_, State::Dead );
           if ( runner_.joinable() )
             runner_.join();
           runner_ = std::thread();
@@ -205,18 +211,27 @@ namespace pgbar {
           };
           auto expected = State::Dormant;
           if ( state_.compare_exchange_strong( expected, desired(), std::memory_order_release ) ) {
+            quota_.store( 0, std::memory_order_release );
+#ifdef __cpp_lib_atomic_wait
+            state_.notify_one();
+#endif
             if PGBAR__CXX17_CNSTXPR ( Mode != Policy::Sync ) {
-              quota_.store( 0, std::memory_order_release );
+#ifdef __cpp_lib_atomic_wait
+              state_.wait( desired(), std::memory_order_acquire );
+#else
               {
                 std::lock_guard<std::mutex> lock { sched_mtx_ };
                 cond_var_.notify_one();
               }
               concurrent::spin_wait(
                 [&]() noexcept { return state_.load( std::memory_order_acquire ) != desired(); } );
+#endif
             } else {
               std::lock_guard<concurrent::SharedMutex> lock1 { res_mtx_ };
               std::lock_guard<std::mutex> lock2 { sched_mtx_ };
+#ifndef __cpp_lib_atomic_wait
               cond_var_.notify_one();
+#endif
               task_();
             }
           }
@@ -229,8 +244,12 @@ namespace pgbar {
           if PGBAR__CXX17_CNSTXPR ( Mode == Policy::Signal ) {
             if ( state_.load( std::memory_order_acquire ) != State::Dormant ) {
               quota_.fetch_add( 1, std::memory_order_release );
+#ifdef __cpp_lib_atomic_wait
+              quota_.notify_one();
+#else
               std::lock_guard<std::mutex> lock { sched_mtx_ };
               cond_var_.notify_one();
+#endif
             }
           } else if PGBAR__CXX17_CNSTXPR ( Mode == Policy::Sync ) {
             PGBAR__ASSERT( state_ == State::Idle );
@@ -244,6 +263,20 @@ namespace pgbar {
         template<Policy Mode>
         void trigger() & noexcept
         {
+#ifdef __cpp_lib_atomic_wait
+          auto state_transfer = [this]( State expected, State desired ) noexcept {
+            if ( concurrent::atomic_commit_one( state_, expected, desired ) )
+              state_.wait( desired, std::memory_order_acquire );
+          };
+          if PGBAR__CXX17_CNSTXPR ( Mode == Policy::Async )
+            state_transfer( State::Loop, State::Warmup );
+          else if PGBAR__CXX17_CNSTXPR ( Mode == Policy::Signal ) {
+            quota_.fetch_add( 1, std::memory_order_release );
+            state_transfer( State::Pulse, State::Primed );
+            quota_.notify_one();
+          } else if PGBAR__CXX17_CNSTXPR ( Mode == Policy::Sync )
+            state_transfer( State::Idle, State::Shot );
+#else
           if PGBAR__CXX17_CNSTXPR ( Mode == Policy::Async ) {
             auto expected = State::Loop;
             if ( state_.compare_exchange_strong( expected, State::Warmup, std::memory_order_release ) )
@@ -271,21 +304,26 @@ namespace pgbar {
                 [this]() noexcept { return state_.load( std::memory_order_acquire ) != State::Shot; } );
             }
           }
+#endif
         }
 
         void abort() noexcept
         {
           auto try_update = [this]( State expected ) noexcept {
-            return state_.compare_exchange_strong( expected, State::Asleep, std::memory_order_release );
+            return concurrent::atomic_commit_one( state_, expected, State::Asleep );
           };
           if ( try_update( State::Warmup ) || try_update( State::Loop ) || try_update( State::Primed )
                || try_update( State::Pulse ) || try_update( State::Shot ) || try_update( State::Idle ) ) {
+#ifdef __cpp_lib_atomic_wait
+            state_.wait( State::Asleep, std::memory_order_acquire );
+#else
             {
               std::lock_guard<std::mutex> lock { sched_mtx_ };
               cond_var_.notify_all();
             }
             concurrent::spin_wait(
               [this]() noexcept { return state_.load( std::memory_order_acquire ) != State::Asleep; } );
+#endif
           }
         }
 
